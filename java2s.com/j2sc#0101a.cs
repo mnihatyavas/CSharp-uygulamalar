d8e5a51fdfc1aeb9c82d98e5e8f0e82d67f09317
M.Nihat Yavaş <46388635+mnihatyavas@users.noreply.github.com>
2023-03-09 02:57:41 +0300

    Add files via upload

---
+++ java2s.com/j2sc#0101a.cs
@@ -0,0 +1,14 @@
+// j2sc#0101a.cs: Ýlk basit bir C# program örneði.
+
+using System;
+//namespace DilTemelleri {
+    class Giriþ1 {
+        public static void Main() {
+            Console.Write (".\nTuþ...");Console.ReadKey();Console.WriteLine ("\n");
+
+            Console.WriteLine ("Basit bir C# programý.");
+
+            Console.Write ("\nTuþ..."); Console.ReadKey();
+        }
+    } 
+//}
